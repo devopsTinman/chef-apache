2a3cc4fd70b134a6b0e754b1a491192994230cf4
Tinman 487 <devops.tinman@gmail.com>
2017-06-15 03:33:12 +0000

    fixed html contents

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,7 +2,8 @@ package 'httpd' do
 	action :install
 end
 
-file '/var/www/index.html' do
+file '/var/www/html/index.html' do
+	content '<html><head>Hello World</head><body>Hello World</body></html>'
 	action :create
 	owner 'root'
 	mode '0655'
@@ -10,5 +11,5 @@ file '/var/www/index.html' do
 end
 
 service 'httpd' do
-	action :start
+	action [ :enable, :start ]
 end
