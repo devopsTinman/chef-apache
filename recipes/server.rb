edbf99f4607ff2ee9b96549019ccb1ec66396f90
Tinman 487 <devops.tinman@gmail.com>
2017-06-27 14:40:38 +0000

    Added cookbook_file home.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,6 +2,10 @@ package 'httpd' do
 	action :install
 end
 
+cookbook_file 'var/www/html/home.html' do
+	source 'home.html'
+end
+
 template '/var/www/html/index.html' do
 	source 'index.html.erb'
 	action :create
@@ -9,6 +13,7 @@ template '/var/www/html/index.html' do
 	mode '0655'
 	group 'root'
 end
+
 
 service 'httpd' do
 	action [ :enable, :start ]
